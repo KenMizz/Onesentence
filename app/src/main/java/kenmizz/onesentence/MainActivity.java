482df764f27d35816fdef2026e59c935a3407fa3
Ken Mizz <mizzken16@gmail.com>
2020-05-23 17:47:49 -0400

    Apply dark mode to dialog

--- app/src/main/java/kenmizz/onesentence/MainActivity.java
+++ app/src/main/java/kenmizz/onesentence/MainActivity.java
@@ -3,14 +3,13 @@ package kenmizz.onesentence;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.content.Intent;
+import android.widget.Toast;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -21,8 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        int DarkMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-        switch(DarkMode) {
+        switch(getDarkMode()) {
             case Configuration.UI_MODE_NIGHT_YES:
                 setTheme(R.style.AppThemeDark);
                 break;
@@ -35,9 +33,24 @@ public class MainActivity extends AppCompatActivity {
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO
+                addMessage();
             }
         });
+    }
+
+    public int getDarkMode() {
+        return getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+    }
+
+    public void addMessage() {
+        MaterialAlertDialogBuilder dialog;
+        if(getDarkMode() == Configuration.UI_MODE_NIGHT_YES) {
+            dialog = new MaterialAlertDialogBuilder(this, R.style.AlertDialogDark);
+        } else {
+            dialog = new MaterialAlertDialogBuilder(this, R.style.AlertDialogLight);
+        }
+        dialog.setTitle(R.string.newsentence)
+                .show();
     }
 
     @Override
@@ -51,16 +64,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int itemId = item.getItemId();
         if(itemId == R.id.option) {
-            new MaterialAlertDialogBuilder(this)
-                    .setTitle(R.string.about)
-                    .setMessage(R.string.about_info)
-                    .setPositiveButton(R.string.confirmButton, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            //pass
-                        }
-                    })
-                    .show();
+            Toast.makeText(this, R.string.about, Toast.LENGTH_SHORT).show();
             return true;
         }
         return super.onOptionsItemSelected(item);
