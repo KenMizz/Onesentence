0ae88bc85e2e7ab04e667fd2ec927c0a11bfb76d
Ken Mizz <mizzken16@gmail.com>
2020-05-24 19:40:23 -0400

    working on RecylerView + CardView

---
+++ app/src/main/java/kenmizz/onesentence/SentenceItem.java
@@ -0,0 +1,14 @@
+package kenmizz.onesentence;
+
+public class SentenceItem {
+
+    private String Sentence;
+
+    public SentenceItem(String sentence) {
+        Sentence = sentence;
+    }
+
+    public String getSentence() {
+        return  Sentence;
+    }
+}
