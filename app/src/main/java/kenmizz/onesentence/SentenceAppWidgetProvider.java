c65b724727764f7e9f18957f66f584b97a2f2603
Ken Mizz <mizzken16@gmail.com>
2020-06-04 18:09:57 -0400

    appWidget

---
+++ app/src/main/java/kenmizz/onesentence/SentenceAppWidgetProvider.java
@@ -0,0 +1,19 @@
+package kenmizz.onesentence;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class SentenceAppWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int widgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.sentence_widget);
+            appWidgetManager.updateAppWidget(widgetId, views);
+        }
+    }
+}
